1aca99b82ba1ce17854e41d25d3bbb5be375978a
Rasheed <rasheedmohammed2006@gmail.com>
2025-07-17 20:56:19 +1000

    Added intial resources for website

---
+++ src/sections/index.js
@@ -0,0 +1,22 @@
+import Services from './Services';
+import Hero from './Hero';
+import PopularProducts from './PopularProducts';
+import SuperQuality from './SuperQuality';
+import SpecialOffers from './SpecialOffers';
+import CustomerReviews from './CustomerReviews';
+import Subscribe from './Subscribe';
+import Footer from './Footer';
+
+export {
+    Hero,
+    Services,
+    PopularProducts,
+    SuperQuality,
+    SpecialOffers,
+    CustomerReviews,
+    Subscribe,
+    Footer
+}
+
+
+
